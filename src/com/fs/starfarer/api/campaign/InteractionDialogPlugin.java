d11e40d988d714e1e97d3049bc8cd3855beb93d2
DesperatePeter <jannes@eckart-ahrensburg.de>
2024-12-16 19:08:24 +0100

    fix: indentation in InteractionDialogPlugin

--- src/com/fs/starfarer/api/campaign/InteractionDialogPlugin.java
+++ src/com/fs/starfarer/api/campaign/InteractionDialogPlugin.java
@@ -18,105 +18,105 @@ import com.fs.starfarer.api.combat.EngagementResultAPI;
  * <p>Example
  * <pre>
  *  <code>
- *		// This is a simple dialog with some text, an image and an option.
- *		// Note: For simplicity sake, error handling and null-checking is omitted in this example *
- *		class MyDialog implements InteractionDialogPlugin{
- *			interface ExecutableOptionData{
- *				void execute();
- *			}
- *			private InteractionDialogAPI dialog = null;
- *			{@code @Override}
- *			void init(InteractionDialogAPI dialog){
- *				this.dialog = dialog;
- *				dialog.getTextPanel().addParagraph("Some text")
- *				dialog.getOptionPanel().addOption(
- *					"OptionName",
- *					new ExecutableOptionData(){
- *						{@code @Override}
- *						void execute(){
- *						  // implementation of what the option should do when clicked
- *						  // For instance, for a leave option:
- *						  dialog.dismiss();
- *						  // If your GUI e.g. moves to another stage after this, you should clear the options and
- *						  // create new options in here. It might also make sense to move this ExecutableOptionData
- *						  // to another file, if the implementation gets too long.
- *						}
- *					}
- *				);
- *				String spriteName = Global.getSettings().getSpriteName("spriteCategory", "mySpriteName");
- *				dialog.getVisualPanel().showImageVisual(
- *					spriteName,
- *					Global.getSettings().getSprite(spriteName).width
- *					Global.getSettings().getSprite(spriteName).height
- *				);
- *			}
- *			{@code @Override}
- *			void optionSelected(String optionText, Object optionData){
- *				ExecutableOptionData option = (ExecutableOptionData)optionData;
- *				if(option == null){ return; }
- *				option.execute();
- *			}
+ * // This is a simple dialog with some text, an image and an option.
+ * // Note: For simplicity sake, error handling and null-checking is omitted in this example
+ * class MyDialog implements InteractionDialogPlugin{
+ *   interface ExecutableOptionData{
+ *     void execute();
+ *   }
+ *   private InteractionDialogAPI dialog = null;
+ *   {@code @Override}
+ *   void init(InteractionDialogAPI dialog){
+ *     this.dialog = dialog;
+ *     dialog.getTextPanel().addParagraph("Some text")
+ *     dialog.getOptionPanel().addOption(
+ *       "OptionName",
+ *       new ExecutableOptionData(){
+ *         {@code @Override}
+ *         void execute(){
+ *           // implementation of what the option should do when clicked
+ *           // For instance, for a leave option:
+ *           dialog.dismiss();
+ *           // If your GUI e.g. moves to another stage after this, you should clear the options and
+ *           // create new options in here. It might also make sense to move this ExecutableOptionData
+ *           // to another file, if the implementation gets too long.
+ *         }
+ *       }
+ *     );
+ *     String spriteName = Global.getSettings().getSpriteName("spriteCategory", "mySpriteName");
+ *     dialog.getVisualPanel().showImageVisual(
+ *       spriteName,
+ *       Global.getSettings().getSprite(spriteName).width
+ *       Global.getSettings().getSprite(spriteName).height
+ *     );
+ *   }
+ *   {@code @Override}
+ *   void optionSelected(String optionText, Object optionData){
+ *     ExecutableOptionData option = (ExecutableOptionData)optionData;
+ *     if(option == null){ return; }
+ *     option.execute();
+ *   }
  *
- *			// Override the remaining methods (Ctrl+O in IntelliJ), but leave their implementation empty if their
- *			// return type is void, or return null otherwise.
+ *   // Override the remaining methods (Ctrl+O in IntelliJ), but leave their implementation empty if their
+ *   // return type is void, or return null otherwise.
  *
- *		}
+ * }
  *  </code>
  *
  * </pre>
  *
  */
 public interface InteractionDialogPlugin {
-	/**
-	 * Initialize your GUI in here, by populating options, texts and images	 *
-	 *
-	 * @param dialog entry point for interacting with the dialog. You should probably store this in a member variable
-	 *
-	 */
-	void init(InteractionDialogAPI dialog);
+  /**
+   * Initialize your GUI in here, by populating options, texts and images   *
+   *
+   * @param dialog entry point for interacting with the dialog. You should probably store this in a member variable
+   *
+   */
+  void init(InteractionDialogAPI dialog);
 
-	/**
-	 * This gets called when the player clicks on an option. Execute the logic for the selected option in here
-	 * @param optionText the name of the option, i.e. the first argument passed to addOption
-	 * @param optionData the data/identifier of the selected option. Cast this to the correct type. Don't forget to
-	 *                   null-check the result of the cast.
-	 */
-	void optionSelected(String optionText, Object optionData);
+  /**
+   * This gets called when the player clicks on an option. Execute the logic for the selected option in here
+   * @param optionText the name of the option, i.e. the first argument passed to addOption
+   * @param optionData the data/identifier of the selected option. Cast this to the correct type. Don't forget to
+   *                   null-check the result of the cast.
+   */
+  void optionSelected(String optionText, Object optionData);
 
-	/**
-	 * Get's called when the player hovers over an option. Can usually be left empty.
-	 * @param optionText
-	 * @param optionData
-	 */
-	void optionMousedOver(String optionText, Object optionData);
+  /**
+   * Get's called when the player hovers over an option. Can usually be left empty.
+   * @param optionText
+   * @param optionData
+   */
+  void optionMousedOver(String optionText, Object optionData);
 
-	/**
-	 * Get's called every frame. Can usually be left empty.
-	 * @param amount
-	 */
-	void advance(float amount);
+  /**
+   * Get's called every frame. Can usually be left empty.
+   * @param amount
+   */
+  void advance(float amount);
 
-	/**
-	 * Get's called after a fleet battle connected to this dialog ends. Can be left empty if the dialog
-	 * you are implementing isn't related to a fleet battle.
-	 *
-	 * This documentation is incomplete.
-	 *
-	 * @param battleResult
-	 */
-	void backFromEngagement(EngagementResultAPI battleResult);
+  /**
+   * Get's called after a fleet battle connected to this dialog ends. Can be left empty if the dialog
+   * you are implementing isn't related to a fleet battle.
+   *
+   * This documentation is incomplete.
+   *
+   * @param battleResult
+   */
+  void backFromEngagement(EngagementResultAPI battleResult);
 
-	/**
-	 * Usually, simply returning null is fine
-	 *
-	 * @return the context, e.g. campaign entity or fleet or other thing that was interacted with to spawn this dialog	 *
-	 */
-	Object getContext();
+  /**
+   * Usually, simply returning null is fine
+   *
+   * @return the context, e.g. campaign entity or fleet or other thing that was interacted with to spawn this dialog   *
+   */
+  Object getContext();
 
-	/**
-	 * Usually, simply returning null or an empty map is fine.
-	 *
-	 * @return a map that maps string keys to memory entries. Only relevant if you want to interact with rulescmd (I think)
-	 */
-	Map<String, MemoryAPI> getMemoryMap();
+  /**
+   * Usually, simply returning null or an empty map is fine.
+   *
+   * @return a map that maps string keys to memory entries. Only relevant if you want to interact with rulescmd (I think)
+   */
+  Map<String, MemoryAPI> getMemoryMap();
 }
