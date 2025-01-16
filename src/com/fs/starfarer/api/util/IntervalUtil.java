9fbc8b3517c071c4e164f30c8a4c49357846c7f0
Wisp <1343347+wispborne@users.noreply.github.com>
2025-01-15 21:35:52 -0800

    IntervalUtil doc.

--- src/com/fs/starfarer/api/util/IntervalUtil.java
+++ src/com/fs/starfarer/api/util/IntervalUtil.java
@@ -2,25 +2,47 @@ package com.fs.starfarer.api.util;
 
 import java.util.Random;
 
+/**
+ * Timer utility that fires at a random interval between minInterval and maxInterval.
+ * <p>
+ * To use, call {@code intervalUtil.advance(float amount);} in an {@code advance(float amount)} method (i.e. every frame).
+ * Then, call {@code intervalUtil.intervalElapsed()} each frame to check if the timer has elapsed.
+ * <p/>
+ * It automatically resets itself after it elapses.
+ *
+ * @param minInterval minimum duration of the timer, in seconds
+ * @param maxInterval maximum duration of the timer, in seconds
+ */
 public class IntervalUtil {
-	
+
 	private float minInterval;
 	private float maxInterval;
 
 	private float currInterval;
 	private float elapsed = 0;
 	private boolean intervalElapsed = false;
-	
+
 	private Random random;
-	
-	public IntervalUtil(float minInterval, float maxInterval) {
-		setInterval(minInterval, maxInterval);
+
+    /**
+     * Timer utility that fires at a random interval between minInterval and maxInterval.
+     * <p>
+     * To use, call {@code intervalUtil.advance(float amount);} in an {@code advance(float amount)} method (i.e. every frame).
+     * Then, call {@code intervalUtil.intervalElapsed()} each frame to check if the timer has elapsed.
+     * <p/>
+     * It automatically resets itself after it elapses.
+     *
+     * @param minInterval minimum duration of the timer, in seconds
+     * @param maxInterval maximum duration of the timer, in seconds
+     */
+    public IntervalUtil(float minInterval, float maxInterval) {
+        setInterval(minInterval, maxInterval);
 	}
-	
+
 	public void forceCurrInterval(float value) {
 		currInterval = value;
 	}
-	
+
 	public Random getRandom() {
 		return random;
 	}
@@ -36,12 +58,12 @@ public class IntervalUtil {
 			advance((float) Math.random() * minInterval);
 		}
 	}
-	
+
 	public void forceIntervalElapsed() {
 		elapsed = currInterval;
 	}
 
-	
+
 	public float getElapsed() {
 		return elapsed;
 	}
@@ -65,7 +87,7 @@ public class IntervalUtil {
 			intervalElapsed = true;
 		}
 	}
-	
+
 	/**
 	 * Returns true once and only once when the current interval is over.  Must be called every frame, otherwise
 	 * the time it returns true might be missed.
@@ -74,7 +96,7 @@ public class IntervalUtil {
 	public boolean intervalElapsed() {
 		return intervalElapsed;
 	}
-	
+
 	public float getIntervalDuration() {
 		return currInterval;
 	}
@@ -96,6 +118,6 @@ public class IntervalUtil {
 	public float getMaxInterval() {
 		return maxInterval;
 	}
-	
-	
+
+
 }
